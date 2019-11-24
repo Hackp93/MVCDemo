520e8dc0f8eb6192d522ee07bd77c5bdb4c4818f
Manu Kumar <manu@neargroup.me>
2019-11-24 14:55:05 +0530

    Initial Commit

---
+++ MVCDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCDemo
+//
+//  Created by Manu Singh on 24/11/19.
+//  Copyright © 2019 hackp93. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
